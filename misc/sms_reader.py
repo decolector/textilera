d37abb2be4cc152aa7b96d3f2f6e6e3b3f857ce9
Camilo Martinez <cmart@decolector.net>
2013-06-19 00:33:14 -0300

    sms over db works now

--- misc/sms_reader.py
+++ misc/sms_reader.py
@@ -11,12 +11,12 @@ class Sms(object):
 class SmsReader():
     def __init__(self):
 
-        self.ADB_PATH = '~/code/android-sdk/platform-tools/adb'
+        self.ADB_PATH = '/Users/cmart/code/android/android-sdk-macosx/platform-tools/adb'
         # avoid using ~/ at the beginning of path
         self.phone = ADB(self.ADB_PATH)
         self.DATABASE_PATH = 'db/mmssms.db'
         self.phone.get_remote_file(
-                '/data/data/com.android.telephony/databases/mmssms.db', 
+                '/data/data/com.android.providers.telephony/databases/mmssms.db', 
                 self.DATABASE_PATH
                 )
 
@@ -37,12 +37,9 @@ class SmsReader():
     #copy sms database
     def update(self):
         self.phone.get_remote_file(
-                '/data/data/com.android.telephony/databases/mmssms.db', 
-                self.DATABASE_PATH
+                '/data/data/com.android.providers.telephony/databases/mmssms.db', 
+                self.DATABASE_PATH  
                 )
-        self.session.begin()
-        #Session = sessionmaker(bind=self.engine)
-        #self.session = Session()
         self.sms = self.session.query(Sms)
         self.messages = [msg.body for msg in self.sms]
         self.sms_num = self.sms.count()
@@ -50,6 +47,7 @@ class SmsReader():
             difference = self.sms_num - self.last_sms_num
             temp_sms = self.sms.slice(self.last_sms_num, self.sms_num).all()
             self.new_sms = [x.body for x in temp_sms]
+            self.last_sms_num = self.sms_num
 
-        self.session.close()     
+        self.session.close() 
 
